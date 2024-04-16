c17c357bce93066fad78981084db428798c9b30a
Roberto Rodriguez <prfiredragon@gmail.com>
2024-04-16 18:29:34 -0400

    Update nano-7.2.sh
    Signed-off-by: Roberto Rodriguez <prfiredragon@gmail.com>

--- rpackmandb/nano/7.2/nano-7.2.sh
+++ rpackmandb/nano/7.2/nano-7.2.sh
@@ -1,3 +1,12 @@
+temppath="./nano-7.2"
+compressfile="./nano-7.2.tar.xz"
+
+cd /tmp
+tar -xf $compressfile
+cd $temppath
+
+
+
 ./configure --prefix=/system/packages/nano     \
             --sysconfdir=/system/packages/nano/etc \
             --enable-utf8     \
@@ -6,3 +15,5 @@ make
 
 make install &&
 install -v -m644 doc/{nano.html,sample.nanorc} /system/packages/nano/share/doc/nano-7.2
+cd ../
+rm -rf $temppath
