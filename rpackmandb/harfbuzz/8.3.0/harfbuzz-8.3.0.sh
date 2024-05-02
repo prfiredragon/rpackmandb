32f1f900be75cb7f816eadb5a884542a2c8be3d6
Roberto Rodriguez <prfiredragon@gmail.com>
2024-05-01 20:49:47 -0400

    added harfbuzz

---
+++ rpackmandb/harfbuzz/8.3.0/harfbuzz-8.3.0.sh
@@ -0,0 +1,25 @@
+temppath="./harfbuzz-8.3.0"
+compressfile="./harfbuzz-8.3.0.tar.xz"
+appath="/system/packages/harfbuzz"
+
+cd /tmp
+tar -xf $compressfile
+cd $temppath
+
+
+
+mkdir build &&
+cd    build &&
+
+meson setup ..            \
+      --prefix=$appath       \
+      --buildtype=release \
+      -Dgraphite2=enabled &&
+ninja
+
+ninja install
+
+
+cd ..
+rm -rf $temppath
+cd ..
