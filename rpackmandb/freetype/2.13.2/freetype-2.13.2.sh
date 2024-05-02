1768368a3b5fc9f7ffbad35bb6a1c0c7ec834cb5
Roberto Rodriguez <prfiredragon@gmail.com>
2024-05-01 21:13:49 -0400

    added freetype-2.13.2

---
+++ rpackmandb/freetype/2.13.2/freetype-2.13.2.sh
@@ -0,0 +1,24 @@
+temppath="./freetype-2.13.2"
+compressfile="./freetype-2.13.2.tar.xz"
+appath="/system/packages/freetype"
+
+cd /tmp
+tar -xf $compressfile
+cd $temppath
+
+
+
+sed -ri "s:.*(AUX_MODULES.*valid):\1:" modules.cfg &&
+
+sed -r "s:.*(#.*SUBPIXEL_RENDERING) .*:\1:" \
+    -i include/freetype/config/ftoption.h  &&
+
+./configure --prefix=$appath --enable-freetype-config --disable-static &&
+make
+
+make install
+
+
+cd ..
+rm -rf $temppath
+cd ..
