d3ec4fe210b692d2ffe49d7fc6861c2576636b6c
Roberto Rodriguez <prfiredragon@gmail.com>
2024-05-21 21:56:33 -0400

    add libnl

---
+++ rpackmandb/libnl/3.9.0/libnl-3.9.0.sh
@@ -0,0 +1,28 @@
+temppath="./libnl-3.9.0"
+compressfile="./libnl-3.9.0.tar.gz"
+appath="/system/packages/libnl"
+
+cd /tmp
+#rm -r $compressfile
+#curl -L https://github.com/thom311/libnl/releases/download/libnl3_9_0/libnl-3.9.0.tar.gz -O
+tar -xf $compressfile
+cd $temppath
+
+
+
+sed -i 's/python/&3/' event_rpcgen.py
+
+
+
+
+./configure --prefix=$appath     \
+            --sysconfdir=/system/os/etc \
+            --disable-static  &&
+make
+
+make install
+
+
+cd ..
+rm -rf $temppath
+cd ..
