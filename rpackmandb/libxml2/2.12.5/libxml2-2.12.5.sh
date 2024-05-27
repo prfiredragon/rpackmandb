357c3b93d6a0fa9804667852dc30b996637ccef2
Roberto Rodriguez <prfiredragon@gmailcom>
2024-05-27 01:05:20 -0400

    fix libs

--- rpackmandb/libxml2/2.12.5/libxml2-2.12.5.sh
+++ rpackmandb/libxml2/2.12.5/libxml2-2.12.5.sh
@@ -55,13 +55,13 @@ export i=$appath
 
                         if [[ "$(/system/packages/coreutils/bin/basename $i)" != "pkgconf" ]]; then
                                 export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$i/lib/pkgconfig
-				ln -s $i/lib/pkgconfig/* /system/packges/pkgconf/lib/pkgconfig/
+				ln -s $i/lib/pkgconfig/* /system/packages/pkgconf/lib/pkgconfig/
                         fi
                 fi
                 if [[ -d "$i/lib64/pkgconfig" && ! -L "$i/lib64/pkgconfig" ]]; then
                                 if [[ "$(/system/packages/coreutils/bin/basename $i)" != "pkgconf" ]]; then
                                                 export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$i/lib64/pkgconfig
-						ln -s $i/lib/pkgconfig/* /system/packges/pkgconf/lib/pkgconfig/
+						ln -s $i/lib64/pkgconfig/* /system/packages/pkgconf/lib/pkgconfig/
                                 fi
                 fi
                 if [[ -d "$i/include" && ! -L "$i/include" ]]; then
